dfe193d29028ecc5f2da7640a838d3b357c1b774
TomHarperDev <18harper.t@lifesciencesutc.net>
2025-02-05 14:11:56 +0000

    submit task

---
+++ ThomasHarper_Cs_Project/Views/SubmitTask.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ThomasHarper_Cs_Project.Views
+{
+    /// <summary>
+    /// Interaction logic for SubmitTask.xaml
+    /// </summary>
+    public partial class SubmitTask : Window
+    {
+        public SubmitTask()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
